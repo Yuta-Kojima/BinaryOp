6da051f9cd85963eddf91be001fa38c0cf0eecab
Yuta Kojima <yuta.k.prg@gmail.com>
2022-01-29 21:21:42 +0900

    プロジェクト ファイルを追加します。

---
+++ BinaryOp/BWriter.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Reflection;
+using System.Runtime;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Linq;
+
+namespace BinaryOp
+{
+    /// <summary>
+    /// オブジェクトの情報をバイナリデータとして書き込むクラス
+    /// </summary>
+    /// <typeparam name="T">書き込む対象のクラス・オブジェクト</typeparam>
+    public class BWriter<T>
+    {
+        public T TargetObject { get; private set; }
+        public BWriter(T targetObject = default)
+        {
+            TargetObject = targetObject;
+        }
+
+        public void Write()
+        {
+            Type typeOfListString = TargetObject.GetType();
+            PropertyInfo[] propertyInfos = typeOfListString.GetProperties();
+            int byteSize = propertyInfos.Sum(item => {
+                if (item.PropertyType.IsValueType)
+                {
+                    return Marshal.SizeOf(item.PropertyType);
+                }
+                else if(item.PropertyType == typeof(string))
+                {
+                    Encoding utf8 = Encoding.UTF8;
+                    string s = (string)item.GetValue(TargetObject)!;
+                    return utf8.GetByteCount(s);
+                }else if (item.PropertyType.IsArray)
+                {
+                    var a = item.GetValue(TargetObject) as Array;
+                    int sum = 0;
+                    foreach(var i in a!)
+                    {
+                        Console.WriteLine(i);
+                        sum += Marshal.SizeOf(i);
+                    }
+                    return sum;
+
+                }
+                return 0;
+            });
+
+            byte[] bytes = new byte[byteSize];
+            int pointer = 0;
+
+            foreach(PropertyInfo propertyInfo in propertyInfos)
+            {
+                var value = propertyInfo.GetValue(TargetObject) ?? new object();
+
+                Console.WriteLine($"{propertyInfo.Name} = ({propertyInfo.PropertyType}) {propertyInfo.GetValue(TargetObject)}");
+                Type type = propertyInfo.PropertyType; 
+                var cnvertedBytes = TypeJudge.TJudge(value);
+                foreach(var cnvByte in cnvertedBytes)
+                {
+                    bytes[pointer++] = cnvByte;
+                }
+            }
+
+            Console.WriteLine(bytes);
+        }
+
+    }
+}
---
+++ BinaryOp/TypeJudge.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.InteropServices;
+
+namespace BinaryOp
+{
+    /// <summary>
+    /// オブジェクトの型に関する判定を行うメソッドをまとめたクラス
+    /// (インスタンス化して使うことはたぶんない)
+    /// </summary>
+    internal class TypeJudge
+    {
+        public static TypeJudge Instance { get; private set; } = new TypeJudge();
+        private TypeJudge()
+        {
+
+        }
+
+        public static byte[] TJudge(object o)
+        {
+
+            switch (o)
+            {
+                case int v:
+                    return BitConverter.GetBytes(v);
+                case long v:
+                    return BitConverter.GetBytes(v);
+                case short v:
+                    return BitConverter.GetBytes(v);
+                case string v:
+                    return Encoding.UTF8.GetBytes(v);
+                default:
+                    if (o.GetType().IsArray)
+                    {
+                        var arr = o as Array;
+                        int l = arr.Length;
+                        int byteSize = Marshal.SizeOf(arr.GetValue(0).GetType());
+                        byte[] b = new byte[l * Marshal.SizeOf(arr.GetValue(0).GetType())];
+                        int pointer = 0;
+                        foreach(var a in arr)
+                        {
+                            foreach(var x in TJudge(a))
+                            {
+                                b[pointer++] = x;
+                            }
+                        }
+                        return b;
+                    }
+                    return null;
+                    break;
+            }
+        }
+    }
+}
